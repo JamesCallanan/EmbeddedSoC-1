a28e102541724dea9817917275125f16e8a10d35
cuandeburca <cuanburke@gmail.com>
2022-03-30 09:23:09 +0100

    Adding display test main file

---
+++ Software/ledDisplayTestMain.c
@@ -0,0 +1,198 @@
+//------------------------------------------------------------------------------------------------------
+// Demonstration program for Cortex-M0 SoC design - simple version, no CMSIS
+// 1)Enable the interrupt for UART - interrupt when character received
+// 2)Go to sleep mode
+// 3)On interruption, echo character back to the UART and collect into buffer
+// 4)When a whole sentence has been received, invert the case and send it back
+// 5)Loop forever doing the above.
+//
+// Version 5 - March 2022
+//------------------------------------------------------------------------------------------------------
+#include <stdio.h>
+#include "DES_M0_SoC.h"
+
+#define BUF_SIZE						100
+#define ASCII_CR						'\r'
+#define CASE_BIT						('A' ^ 'a')
+#define nLOOPS_per_DELAY		1000000
+
+#define INVERT_LEDS					(GPIO_LED ^= 0xff)
+
+#define ARRAY_SIZE(__x__)   (sizeof(__x__)/sizeof(__x__[0]))
+
+volatile uint8  counter  = 0; // current number of char received on UART currently in RxBuf[]
+volatile uint8  BufReady = 0; // Flag to indicate if there is a sentence worth of data in RxBuf
+volatile uint8  RxBuf[BUF_SIZE];
+
+//////////////////////////////////////////////////////////////////
+// Interrupt service routine for System Tick interrupt
+//////////////////////////////////////////////////////////////////
+void SysTick_ISR()	
+{
+	// Do nothing - this interrupt is not used here
+}
+
+// function to display temperature code
+void dispTempLED(uint8 number)
+{
+	switch(number)
+	{
+		case 0:
+			GPIO_LED = 0x0000;
+			break;
+			
+		case 1:
+			GPIO_LED = 0x0001;
+			break;
+		
+		case 2:
+			GPIO_LED = 0x0003;
+			break;
+			
+		case 3:
+			GPIO_LED = 0x0007;
+			break;
+			
+		case 4:
+			GPIO_LED = 0x000F;
+			break;
+			
+		case 5:
+			GPIO_LED = 0x001F;
+			break;
+			
+		case 6:
+			GPIO_LED = 0x003F;
+			break;
+			
+		case 7:
+			GPIO_LED = 0x007F;
+			break;
+			
+		case 8: 
+			GPIO_LED = 0x00FF;
+			break;
+			
+		case 9:
+			GPIO_LED = 0x01FF;
+			break;
+			
+		case 10:
+			GPIO_LED = 0x03FF;
+			break;
+			
+		case 11:
+			GPIO_LED = 0x07FF;
+			break;
+			
+		case 12:
+			GPIO_LED = 0x0FFF;
+			break;
+			
+		case 13:
+			GPIO_LED = 0x1FFF;
+			break;
+			
+		case 14:
+			GPIO_LED = 0x3FFF;
+			break;
+			
+		case 15:
+			GPIO_LED = 0x7FFF;
+			break;
+			
+		case 16:
+			GPIO_LED = 0xFFF;
+			break;
+			
+		default:
+			GPIO_LED = 0x0000;
+			break;
+}
+
+// function which maps from integer to 8 bit hex for 7 segment display
+uint8 map2segDisp(uint8 digit)
+{
+	uint8 raw;
+	switch(digit)
+	{
+		case 0:
+			raw = 0xFA;
+			break;
+			
+		case 1:
+			raw = 0x60;
+			break;
+			
+		case 2:
+			raw = 0xDA;
+			break;
+			
+		case 3:
+			raw = 0xF2;
+			break;
+			
+		case 4:
+			raw = 0x66;
+			break;
+			
+		case 5:
+			raw = 0xB6;
+			break;
+			
+		case 6:
+			raw = 0xBE;
+			break;
+			
+		case 7:
+			raw = 0xE0;
+			break;
+			
+		case 8:
+			raw = 0xFE;
+			break;
+			
+		case 9:
+			raw = 0xF6;
+			
+		default:
+			raw = 0x00;
+	}
+	return raw;
+}
+	
+	
+//////////////////////////////////////////////////////////////////
+// Software delay function
+// As a rough guide, looping 1000 times takes 160 us to 220 us, 
+// depending on the compiler optimisation level.
+//////////////////////////////////////////////////////////////////
+void delay(uint32 n) 
+{
+	volatile uint32 i;
+	for(i=0;i<n;i++)
+	{
+	}
+}
+
+
+//////////////////////////////////////////////////////////////////
+// Main Function
+//////////////////////////////////////////////////////////////////
+int main(void) 
+{
+	uint8 i;
+
+	while(1)		// loop forever
+	{	
+		// loop through the temperature LED code indefinitely
+		for(i=0;i<n;i++)
+		{
+			// display temperature code for 
+			dispTempLED(i);
+			delay(4000);
+		}
+
+	} // end of infinite loop
+
+}  // end of main
